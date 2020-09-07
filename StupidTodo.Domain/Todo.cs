08f70ea2cd7f03ee7c8ac7995e96b364dd5d248b
Dane Vinson <dane.vinson@sprbrk.com>
2020-09-06 22:12:31 -0700

    Enabled nullable reference type checking for the solution

--- StupidTodo.Domain/Todo.cs
+++ StupidTodo.Domain/Todo.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace StupidTodo.Domain
 {
     public class Todo
     {
-        public string Description { get; set; }
+        public Todo()
+        { }
+
+        public Todo(string description, string userid) => 
+            (Description, UserId) = (description, userid);
+
+        public string Description { get; set; } = string.Empty;
         public bool Done { get; set; }
-        public string Id { get; set; }
-        public string UserId { get; set; }
+        public string Id { get; set; } = string.Empty;
+        public string UserId { get; set; } = string.Empty;
     }
 }
